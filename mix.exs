421d57925a2886349e9dd6054a8823e6b71ce79f
dev-rroslan <dev.rroslan@gmail.com>
2025-04-17 12:29:07 +0800

    add resend to runtime and edit user notifier

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule HeadsUp.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:resend, "~> 0.4.4"}
     ]
   end
 
